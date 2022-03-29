c13c8f5dd1dfc81f12894a2d72f5740e383aeb5e
Baedonguri <71864367+baedonguri@users.noreply.github.com>
2022-03-30 00:47:57 +0900

    debug

--- HotMusic/app.py
+++ HotMusic/app.py
@@ -7,6 +7,8 @@ import hashlib # sha256을 사용하기 위한 모듈
 from pymongo import MongoClient
 
 app = Flask(__name__)
+app.secret_key = b'xr43321\n\xec'
+
 
 client = MongoClient('localhost', 27017)
 db = client.dbHotMusic
@@ -14,10 +16,7 @@ db = client.dbHotMusic
 # JWT 토큰을 만들기 위한 비밀키값입니다.
 # 아무거나 넣어도 상관없음
 # 서버만 알고 있기 때문에, 내 서버 안에서 인코딩(암호화), 디코딩(복호화)를 할 수 있습니다.
-app.secret_key = b'xr43321\n\xec'
-
-admin_id = 'admin'
-admin_pw = '1234'
+SECRET_KEY = 'JungleHotMusic'
 
 # 임시 추가
 @app.route('/')
@@ -46,9 +45,13 @@ def api_register():
     id_receive = request.form['id_give']
     pw_receive = request.form['pw_give']
 
-    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+    register_check = db.user.find_one({'id':id_receive})
 
-    db.user.insert_one({'id':id_receive, 'pw':pw_hash})
+    if register_check != None:
+        return jsonify({'result':'fail', 'msg':'이미 존재하는 아이디입니다.'})
+    else:
+        pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+        db.user.insert_one({'id':id_receive, 'pw':pw_hash})
 
     return jsonify({'result' : 'success'})
 
@@ -58,8 +61,38 @@ def api_login():
     id_receive = request.form['id_give']
     pw_receive = request.form['pw_give']
 
-    print(id_receive, pw_receive)
-    return jsonify({'result' : 'success'})
+    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+
+    login_check = db.user.find_one({'id':id_receive, 'pw':pw_hash})
+
+    if login_check != None:
+        payload = {
+            'id' : id_receive,
+            'exp' : datetime.datetime.utcnow() + datetime.timedelta(seconds=10)
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+        return jsonify({'result':'success', 'token':token})
+    else:
+        return jsonify({'result':'fail', 'msg':'아이디/비밀번호가 일치하지 않습니다.'})
+
+@app.route('/user', methods=['GET'])
+def api_valid():
+    token_receive = request.headers['token_give']
+
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        # userinfo = db.user.find_one({'id':payload['id']},{'_id':0})
+        userinfo = db.user.find_one({'id':payload['id']})
+
+
+        # datas = list(db.playlist.find({'id':userinfo['id']}))
+
+        return jsonify({'result':'success','name':userinfo['id']})
+
+    except jwt.ExpiredSignatureError:
+        return jsonify({'result':'fail','msg':'로그인이 만료되었습니다.'})
+
 
 @app.route('/delete', methods=['POST'])
 def api_delete():
